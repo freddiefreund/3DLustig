d14597b51fb619faa8335497773290de0e3256a5
Sebastian Thuemmel <sebastianthuemmel@yahoo.ca>
2021-02-20 21:56:50 +0100

    add throw script

---
+++ Assets/Scripts/Throw.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Throw : MonoBehaviour
+{
+    [SerializeField] Transform camera;
+    [SerializeField] GameObject ball;
+    float timeStamp = 0;
+    bool mouseHeld = false;
+    float shootPower;
+    
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            timeStamp = Time.time;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            print(camera.forward);
+            shootPower = Time.time - timeStamp;
+            shootPower = Mathf.Clamp(shootPower, 0.1f, 2f);
+            var spawnLocation = camera.position + camera.forward * 1;   
+            var obj = Instantiate(ball, spawnLocation, camera.rotation);
+            obj.GetComponent<Rigidbody>().velocity = camera.forward * shootPower * 50;
+        }
+    }
+}
