473d6c4d7adf4835efb8845f1a5ce18ea81b9ee7
Freddie <frederickfreund@gmail.com>
2021-02-20 21:29:31 +0100

    Level und CollisionAreas added

---
+++ Assets/GoalCollisionHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalCollisionHandler : MonoBehaviour
+{
+    [SerializeField] private int scoreValue;
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("You Scored: " + scoreValue + " Points!");
+        }
+    }
+}
