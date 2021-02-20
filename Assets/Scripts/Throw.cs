a7489c79567e17c10182e7ee9305729e514a7710
Freddie <frederickfreund@gmail.com>
2021-02-20 22:55:47 +0100

    add crosshair changes

--- Assets/Scripts/Throw.cs
+++ Assets/Scripts/Throw.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Throw : MonoBehaviour
 {
@@ -9,24 +10,49 @@ public class Throw : MonoBehaviour
     [SerializeField] GameObject ball = default;
     
     [SerializeField] float shootPowerMultiplier = 1f;
-    [SerializeField] float maxChargeTime = 2f;
+    [SerializeField] float maxChargeTime = 1.5f;
+
+    [SerializeField] private Image crosshair;
     
     float timeStamp = 0;
-    
+    private const float defaultCrosshairAlpha = 0.1f;
+    private bool clicking = false;
+    private float shootPower;
+
+    private void Start()
+    {
+        setCrosshairAlpha();
+    }
+
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
             timeStamp = Time.time;
+            clicking = true;
+        }
+        
+        if (clicking)
+        {
+            shootPower = Time.time - timeStamp;
+            shootPower = Mathf.Clamp(shootPower, 0.1f, maxChargeTime) * 1.5f;
+            setCrosshairAlpha(shootPower / 3);
         }
 
         if (Input.GetMouseButtonUp(0))
         {
-            float shootPower = Time.time - timeStamp;
-            shootPower = Mathf.Clamp(shootPower, 0.1f, maxChargeTime);
+            clicking = false;
             var spawnLocation = cam.position + cam.forward * shootPowerMultiplier;   
             var obj = Instantiate(ball, spawnLocation, cam.rotation);
             obj.GetComponent<Rigidbody>().velocity = cam.forward * shootPower * 50;
+            setCrosshairAlpha();
         }
+    }
+
+    void setCrosshairAlpha(float alpha = 0f)
+    {
+        Color c = crosshair.color;
+        c.a = alpha + defaultCrosshairAlpha;
+        crosshair.color = c;
     }
 }
